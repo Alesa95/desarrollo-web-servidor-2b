e851b5e1ecd92b5e5d1608f563b0e9fd708135a4
Alesa95 <alejandra_santiago_95@hotmail.com>
2023-12-15 11:48:33 +0100

    primer commit

---
+++ bases_de_datos/view_book.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php require 'database_conection.php' ?>
+</head>
+<body>
+    <?php
+    if(!isset($_GET["titulo"])) header('location: index.php');
+    
+    if($_SERVER["REQUEST_METHOD"] == "GET") {
+        $titulo = $_GET["titulo"];
+        
+        $sql = $_conexion -> prepare("SELECT * FROM libros WHERE titulo = ?");
+        $sql -> bind_param("s", $titulo);
+        $sql -> execute();
+        $resultado = $sql -> get_result();
+        $sql -> close();
+        $fila = $resultado -> fetch_assoc();
+    }
+    ?>
+    <div class="container">
+        <h3>Título: <?php echo $fila["titulo"] ?></h3>
+        <h3>Páginas: <?php echo $fila["paginas"] ?></h3>
+        <h3>Autor/a: <?php echo $fila["autor"] ?></h3>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
